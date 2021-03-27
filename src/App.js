be2c2d56c32968f04250ea2d20892153dd877f16
Denys Tymchenko <66609114+dennistim@users.noreply.github.com>
2021-03-27 19:03:42 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,156 @@
+import React, { useState } from "react";
+import "./App.css";
+import { v1 as uid } from "uuid";
+import ListItem from "./components/ListItem";
+import Button from "./components/Button";
+import Container from "./components/Container";
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleOnChange = (e) => setName(e.target.value);
+
+  const changeElementOrder = (array, id, moveTo = "up") => {
+    const fromIndex = array.findIndex((item) => item.id === id);
+    const toIndex =
+      moveTo === "up" ? fromIndex - 1 : moveTo === "down" ? fromIndex + 1 : 1;
+    return changeElementIndexInArray([...array], fromIndex, toIndex);
+  };
+
+  const changeElementOrderInNestedList = (array, id, parentId, moveTo = "up") =>
+    array.map((item) => {
+      if (item.id === parentId) {
+        const updatedSubList = changeElementOrder(item.subList, id, moveTo);
+
+        return {
+          ...item,
+          subList: updatedSubList
+        };
+      }
+      return {
+        ...item,
+        ...(item.subList && {
+          subList: changeElementOrderInNestedList(
+            item.subList,
+            id,
+            parentId,
+            moveTo
+          )
+        })
+      };
+    });
+
+  const addElementToSubList = (array, clickedId, newItem) =>
+    array.map((item) => {
+      if (item.id === clickedId) {
+        return {
+          ...item,
+          subList: [...item.subList, newItem]
+        };
+      }
+      return {
+        ...item,
+        ...(item.subList && {
+          subList: addElementToSubList(item.subList, clickedId, newItem)
+        })
+      };
+    });
+
+  const changeElementIndexInArray = (arr, fromIndex, toIndex) => {
+    const element = arr[fromIndex];
+    arr.splice(fromIndex, 1);
+    arr.splice(toIndex, 0, element);
+    return arr;
+  };
+
+  const deleteElementFromList = (array, id, parentId) =>
+    array.map((item) => {
+      if (item.id === parentId) {
+        const updatedSubList = item.subList.filter(
+          (listItem) => listItem.id !== id
+        );
+        return {
+          ...item,
+          subList: updatedSubList
+        };
+      }
+      return {
+        ...item,
+        ...(item.subList && {
+          subList: deleteElementFromList(item.subList, id, parentId)
+        })
+      };
+    });
+
+  const handleChangeOrder = (id, parentId, moveTo) => {
+    let updatedList;
+    if (!parentId) updatedList = changeElementOrder(list, id, moveTo);
+    else
+      updatedList = changeElementOrderInNestedList(list, id, parentId, moveTo);
+    setList(updatedList);
+  };
+
+  const onAdd = (clickedId, parentId, title, isTopLevel) => {
+    if (!title) return;
+
+    const newList = {
+      id: uid(),
+      parentId: clickedId ? clickedId : null,
+      title: title,
+      subList: []
+    };
+
+    if (isTopLevel) {
+      setName("");
+      setList([...list, newList]);
+      return;
+    }
+
+    const updatedList = addElementToSubList(list, clickedId, newList);
+    setList(updatedList);
+  };
+
+  const onDelete = (id, parentId) => {
+    let updatedList;
+    if (!parentId) updatedList = list.filter((listItem) => listItem.id !== id);
+    else updatedList = deleteElementFromList(list, id, parentId);
+    setList(updatedList);
+  };
+
+  const getListItem = (listItem, disableDown, disabledUp) => (
+    <ListItem
+      key={listItem.id}
+      listItem={listItem}
+      getListItem={getListItem}
+      onAdd={onAdd}
+      onChangeOrder={handleChangeOrder}
+      onDelete={onDelete}
+      disableDown={disableDown}
+      disabledUp={disabledUp}
+    />
+  );
+
+  return (
+    <Container>
+      <ul>
+        {list.map((listItem, index) =>
+          getListItem(listItem, index === list.length - 1, index === 0)
+        )}
+      </ul>
+      <div>
+        <input
+          value={name}
+          onChange={handleOnChange}
+          type="text"
+          placeholder="New list"
+        />
+        <Button onClick={() => onAdd(null, null, name, true)} type="primary">
+          Add
+        </Button>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/ListItem.js
@@ -0,0 +1,95 @@
+import React, { useState, memo } from "react";
+import Button from "./Button";
+
+const ListItem = memo(
+  ({
+    listItem,
+    getListItem,
+    onDelete,
+    onAdd,
+    onChangeOrder,
+    disableDown,
+    disabledUp
+  }) => {
+    const [hidden, setHidden] = useState(false);
+    const [name, setName] = useState("");
+
+    const hide = () => setHidden(!hidden);
+
+    const handleOnChange = (e) => setName(e.target.value);
+
+    return (
+      <>
+        <li className="container flexBox">
+          <div className="flexBox">
+            <p>{listItem.title}</p>
+          </div>
+          <div className="flexBox">
+            <div className="flexBox">
+              <Button
+                type="primary"
+                disabled={disabledUp}
+                onClick={() =>
+                  onChangeOrder(listItem.id, listItem.parentId, "up")
+                }
+              >
+                up
+              </Button>
+              <Button
+                type="primary"
+                disabled={disableDown}
+                onClick={() =>
+                  onChangeOrder(listItem.id, listItem.parentId, "down")
+                }
+              >
+                down
+              </Button>
+              <Button
+                type="danger"
+                onClick={() => onDelete(listItem.id, listItem.parentId)}
+              >
+                delete
+              </Button>
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="New Item"
+                id={listItem.id}
+                onChange={handleOnChange}
+                value={name}
+              />
+              <Button
+                type="secondary"
+                onClick={() => {
+                  onAdd(listItem.id, listItem.parentId, name);
+                  setName("");
+                }}
+              >
+                add sub item
+              </Button>
+            </div>
+          </div>
+        </li>
+
+        {listItem.subList.length > 0 && (
+          <button onClick={hide} className="hasSubList" />
+        )}
+        {!hidden && listItem.subList && listItem.subList.length > 0 && (
+          <ul>
+            {listItem.subList.map((item, index) =>
+              getListItem(
+                item,
+                index === listItem.subList.length - 1 ||
+                  listItem.subList.length === 1,
+                index === 0
+              )
+            )}
+          </ul>
+        )}
+      </>
+    );
+  }
+);
+
+export default ListItem;
